6aa334999d8e4c8b98c62b5628ec4ce8274921e8
frncisadrian <2015081640@ust-ics.mygbiz.com>
2018-09-01 09:45:20 +0800

    Initial commit

---
+++ app/src/main/java/francisadrian/lezada/com/lezadafrancisadrianlab1/ics115_lab2_linear.java
@@ -0,0 +1,13 @@
+package francisadrian.lezada.com.lezadafrancisadrianlab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ics115_lab2_linear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ics115_lab2_linear);
+    }
+}
